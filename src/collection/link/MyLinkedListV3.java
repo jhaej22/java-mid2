b15c129ba11c82d2b556bdf631ae293f28247975
haejoon <jhaej0722@gmail.com>
2024-08-10 12:55:22 +0900

    commit and push

---
+++ src/collection/link/MyLinkedListV3.java
@@ -0,0 +1,129 @@
+package collection.link;
+
+public class MyLinkedListV3<E> {
+
+    private Node<E> first;
+    private int size = 0;
+
+    public void add(E e) {
+        Node<E> newNode = new Node<>(e);
+        if (first == null) {
+            first = newNode;
+        } else {
+            Node<E> lastNode = getLastNode();
+            lastNode.next = newNode;
+        }
+        size++;
+    }
+
+    private Node<E> getLastNode() {
+        Node<E> x = first;
+        while (x.next != null) {
+            x = x.next;
+        }
+        return x;
+    }
+
+    // 코드 추가
+    public void add(int index, E e) {
+        Node<E> newNode = new Node<>(e); // x04
+        if (index == 0) {
+            newNode.next = first; // x01
+            first = newNode; // x04
+        } else {
+            Node<E> prev = getNode(index - 1);
+            newNode.next = prev.next;
+            prev.next = newNode;
+        }
+        size++;
+    }
+
+    public E set(int index, E element) {
+        Node<E> x = getNode(index);
+        E oldValue = x.item;
+        x.item = element;
+        return oldValue;
+    }
+
+    // 코드 추가
+    public E remove(int index) {
+        Node<E> removeNode = getNode(index);
+        E removedItem = removeNode.item;
+        if (index == 0) {
+            first = removeNode.next;
+        } else {
+            Node<E> prev = getNode(index - 1);
+            prev.next = removeNode.next;
+        }
+        removeNode.item = null;
+        removeNode.next = null;
+        size--;
+        return removedItem;
+    }
+
+    private Node<E> getNode(int index) {
+        Node<E> x = first;
+        for (int i = 0; i < index; i++) {
+            x = x.next;
+        }
+        return x;
+    }
+
+    public E get(int index) {
+        Node<E> node = getNode(index);
+        return node.item;
+    }
+
+    public int indexOf(E o) {
+        int index = 0;
+        for (Node<E> x = first; x != null; x = x.next) {
+            if (o.equals(x.item)) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyLinkedListV1{" +
+                "first=" + first +
+                ", size=" + size +
+                '}';
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+
+        public Node(E item) {
+            this.item = item;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            Node<E> x = this;
+            sb.append("[");
+            while (x != null) {
+                sb.append(x.item);
+                if (x.next != null) {
+                    sb.append("->");
+                }
+                x = x.next;
+            }
+            sb.append("]");
+            return sb.toString();
+    }
+  }
+}
+// MyLinkedListV3<E>로 제네릭 클래스로 선언
+// Object로 처리하던 부분을 타입 매개변수 <E>로 변경
+// Node 클래스를 외부 클래스에서 정적 중첩 클래스로 변경
+// Node<E>도 제네릭 클래스로 선언함
+
